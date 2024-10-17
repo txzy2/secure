73164b5ea68196418d667fc48a5f8bd01e8353c9
Anton Kamaev <99755906+kamaeff@users.noreply.github.com>
2024-10-17 19:26:36 +0300

    Updates

--- assets/FooterTemplate-CnvFMmO4.js
+++ assets/FooterTemplate-CnvFMmO4.js
@@ -1,4 +1,4 @@
-import{q as et,d as Et,r as Vt,v as ot,x as Lt,o as zt,y as Ft,a as xt,j as Rt,w as Ut,z as Bt,A as qt,u as C,k as $t,c as kt,b as E,e as N,p as Kt,f as Nt,g as Wt}from"./index-C7_stpLu.js";/**
+import{q as et,d as Et,r as Vt,v as ot,x as Lt,o as zt,y as Ft,a as xt,j as Rt,w as Ut,z as Bt,A as qt,u as C,k as $t,c as kt,b as E,e as N,p as Kt,f as Nt,g as Wt}from"./index-BWBqOceH.js";/**
  * @license lucide-vue-next v0.427.0 - ISC
  *
  * This source code is licensed under the ISC license.
